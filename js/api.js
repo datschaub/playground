2df37db9be58d793c0ede531f6f8f8c0be4a0a8c
datschaub <linus__k@hotmail.com>
2016-11-22 20:33:31 +0100

    Added html for API

---
+++ js/api.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+
+	var rest = new Vue({
+
+		el: '#rest',
+
+		data:
+		{
+			title: 'RESTful :)',
+			dataInput: ''
+		},
+
+		methods:
+		{
+			postItem: function(){
+
+			},
+
+			getItem: function(){
+
+			}
+		}
+	})
+
+}
+
+	
